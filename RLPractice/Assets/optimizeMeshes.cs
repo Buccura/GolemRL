a2025bf7628807ee0208d09ca6d30a34090e0022
Buccura <jgd.2@juno.com>
2017-03-06 15:32:53 -0800

    Updated Doodad system

---
+++ RLPractice/Assets/optimizeMeshes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class optimizeMeshes : MonoBehaviour {
+
+
+    public void CombineMeshes()
+    {
+        MeshFilter[] mFilt = GetComponentsInChildren<MeshFilter>() ;
+        CombineInstance[] combine = new CombineInstance[mFilt.Length];
+        for(int i = 0; i < mFilt.Length;i++)
+        {
+            combine[i].mesh = mFilt[i].sharedMesh;
+            combine[i].transform = mFilt[i].transform.localToWorldMatrix;
+            if(mFilt[i].gameObject.tag == "Wall")
+            {
+                mFilt[i].gameObject.active = false;
+            }
+         //   mFilt[i].gameObject.active = false;
+        }
+
+        transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine, false);
+        transform.gameObject.active = true;
+
+    }
+}
