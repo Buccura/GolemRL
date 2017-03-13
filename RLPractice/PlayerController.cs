dfacc109c75308ee858e9680af337f4bea629940
Ryan Williams <Bumber64@users.noreply.github.com>
2017-03-13 15:27:15 -0700

    Sort of working PlayerControler

--- RLPractice/PlayerController.cs
+++ RLPractice/PlayerController.cs
@@ -1,36 +1,47 @@
 //GolemRL Player Object
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
-{	public double player_speed = 100.0; //Move speed
-	public double player_turn_rate = 90.0; //Turn speed (degrees/sec)
-	public double player_projectile_speed = 200.0; //Projectile muzzel velocity
-	public Projectile basic_bullet;
-	
+{	public float player_speed = 100.0f; //Move speed
+	public float player_turn_rate = 90.0f; //Turn speed (degrees/sec)
+	public float player_projectile_speed = 200.0f; //Projectile muzzel velocity
+	public GameObject basic_bullet;
+
+	void Start()
+	{	
+	}
+
 	void Update()
 	{	//Movement
-		double x_mov = Input.GetAxis("Horizontal") * Time.deltaTime * player_speed;
-		double z_mov = Input.GetAxis("Vertical") * Time.deltaTime * player_speed;
-		
+		float x_mov = Input.GetAxis("Horizontal") * Time.deltaTime * player_speed;
+		float z_mov = Input.GetAxis("Vertical") * Time.deltaTime * player_speed;
+		Vector3 aim_point;
+
 		//Aiming
-		Vector3 aim_point Camera.ScreenToWorldPoint(Input.mousePosition); //Pointer location
-		Vector3 aim_displace = aim_point - transform.Position; //Displacement from player to pointer
-		Vector3 player_heading = transform.Forward;
-		double aim_angle = Math.Atan2(aim_displace.x,aim_displace.z) - Math.Atan2(player_heading.x,player_heading.z);
-		aim_angle *= 180.0 / Math.PI; //Desired rotation change in degrees
-		double turn_angle = Math.Sign(aim_angle) * player_turn_rate * Time.deltaTime; //Maximum possible turn this tick
-		if( Math.Abs(aim_angle) < player_turn_rate*Time.deltaTime ) //If desired rotation less than maximum
+		if (Camera.main != null)
+		{	aim_point = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Pointer location
+		}
+		else
+		{	aim_point = transform.forward;
+		}
+		Vector3 aim_displace = aim_point - transform.position; //Displacement from player to pointer
+		Vector3 player_heading = transform.forward;
+		float aim_angle = Mathf.Atan2(aim_displace.x,aim_displace.z) - Mathf.Atan2(player_heading.x,player_heading.z);
+		aim_angle *= 180.0f / Mathf.PI; //Desired rotation change in degrees
+		float turn_angle = Mathf.Sign(aim_angle) * player_turn_rate * Time.deltaTime; //Maximum possible turn this tick
+		if( Mathf.Abs(aim_angle) < player_turn_rate*Time.deltaTime ) //If desired rotation less than maximum
 		{	turn_angle = aim_angle; //Then don't overshoot
 		}
-		
+
 		//Update transforms
-		transform.Translate(x_mov, 0.0, z_mov);
-		transform.Rotate(0.0, turn_angle, 0);
-		
+		transform.Translate(x_mov, 0.0f, z_mov);
+		transform.Rotate(0.0f, turn_angle, 0.0f);
+
 		//Firing
 		if ( Input.GetMouseButtonDown(0) )
-		{	Projectile proj = Instantiate(basic_bullet, transform.position, transform.rotation);
-			proj.rigidbody.AddForce( Vector3(x_mov, 0.0, z_mov) + transform.forward*player_projectile_speed );
+		{	GameObject shot = Instantiate(basic_bullet, transform.position, transform.rotation);
+			shot.GetComponent<Rigidbody>().AddForce( new Vector3(x_mov, 0.0f, z_mov) + transform.forward*player_projectile_speed );
 		}
 	}
 }
