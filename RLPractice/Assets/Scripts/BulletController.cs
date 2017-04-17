f35b7c94cbae5e8322bc6fe099861e3ecb48eef1
Bumber64 <ryan_williams@csu.fullerton.edu>
2017-04-17 14:39:25 -0700

    Working guns

--- RLPractice/Assets/Scripts/BulletController.cs
+++ RLPractice/Assets/Scripts/BulletController.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour {
 	public float lifetime = 4.0f; //Time to live in seconds
+	public int damage = 0; //Damage in hitpoints
 	public Vector3 bullet_vel;
 
+	//private Rigidbody rb;
+
 	void Start()
-	{	
+	{	//rb = GetComponent<Rigidbody> ();
 	}
 	
 	void Update()
@@ -16,4 +19,8 @@ public class BulletController : MonoBehaviour {
 		}
 		transform.Translate(bullet_vel*Time.deltaTime, Space.World);
 	}
+
+	/*void FixedUpdate()
+	{	rb.velocity = bullet_vel*Time.deltaTime;
+	}*/
 }
