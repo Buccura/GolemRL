6947ef8859c041b447f507b059c6ea407d8bd1d0
Buccura <jgd.2@juno.com>
2017-05-07 13:14:55 -0700

    Updating the lappy

---
+++ RLPractice/Assets/env_Wall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class env_Wall : MonoBehaviour {
+
+    public bool startingWall;
+    public bool isEnabled;
+
+   
+
+
+}
---
+++ RLPractice/Assets/menu_Main.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menu_Main : MonoBehaviour {
+
+    public Button start, exit;
+
+    void Start()
+    {
+        Debug.Log(start.transform.position);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void ExitGame()
+    {
+
+    }
+}
